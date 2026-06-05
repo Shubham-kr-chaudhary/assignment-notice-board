8340d95469c4b359485ae7c44a966553db241f46
Shubham Kumar Chaudhary <shubhamkumchaudhary@gmail.com>
2026-06-05 17:58:17 +0530

    implement get and post notices api

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,22 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+const connectionString = process.env.DATABASE_URL!;
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient();
+  global.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  global.prisma = prisma;
 }
